c4159c74f3cf8e9504337d83f3ca491881d1f176
seanauer42 <44852194+seanauer42@users.noreply.github.com>
2023-09-30 15:42:22 -0400

    Update sidebar.js
    finished putting links in alphabetical order

--- sidebar.js
+++ sidebar.js
@@ -27,50 +27,18 @@ s.innerHTML = `<br><a href="../767-reccurent">767 Reccurrent</a>
    	<br><hr><a href="../hazardous-awareness">Hazardous Awareness</a>
    	<br><hr><a href="../heat-illness-prevention-plan">Heat Illness Prevention</a>
    	<br><hr><a href="../information-security-and-data-privacy">Information Security and Data Privacy</a>
-
-    	<br><hr><a href="../rvsm-awareness">RVSM Awareness</a>
 	<br><hr><a href="../llm-awareness">LLM Awareness</a>
-	<br><hr><a href="../etops-awareness">ETOPS Awareness</a>
-
-	<br><a href="../dangerous-goods-awareness">Dangerous Goods Awareness</a>
-	
-	<br><hr><a href="../environmental-above-ground-storage-tanks">Environmental Above Ground Storage Tanks</a>
-	<br><hr><a href="../technician-environmental-training">Technician Environmental Trianing</a>
-	<br><hr><a href="../spill-response-and-water-protection">Spill Response and Water Protection</a>
-	
 	<br><hr><a href="../managing-waste">Managing Waste</a>
-
- 	<br><hr><a href="../melcdl">MEL/CDL</a>
-		
 	<br><hr><a href="../personal-protective-equipment">Personal Protective Equipment</a>
-	<br><hr><a href="../fall-restraint">Fall Restraint</a>
+	<br><hr><a href="../rii-far-initial">RII FAR Initial</a>
+	<br><hr><a href="../rii-recurrent">RII Recurrent</a>
+	<br><hr><a href="../rvsm-awareness">RVSM Awareness</a>
 	<br><hr><a href="../scaffold-training">Scaffold Training</a>
-	<br><hr><a href="../harness-lanyard-and-srl-prior-to-use-inspection">Harness Lanyard and SRL Prior to Use Inspection</a>
 	<br><hr><a href="../sling-inspection">Sling Inspection</a>
-	
-	<br><hr><a href="../ewis">EWIS</a>
-	<br><hr><a href="../ewis-housekeeping">EWIS Housekeeping</a>
-	<br><hr><a href="../ewis-inspection-and-human-factors">EWIS Inspection and Human Factors</a>
-	<br><hr><a href="../ewis-wiring-practices">EWIS Wiring Practices</a>
-	<br><hr><a href="../general-ewis-practices">General EWIS Practices</a>
-	<br><hr><a href="../ewis-introduction">EWIS Introduction</a>
-	
-	<br><hr><a href="../aml-eml-training">AML/EML Training</a>
-	
-	<br><hr><a href="../harness-lanyard-and-srl-prior-to-use-inspection">Harness Lanyard and SRL Prior to use Inspection</a>
-	
-	<br><hr><a href="../personal-protective-equipment">Personal Protective Equipment</a>
-	
-	<br><hr><a href="../fod-awareness">FOD Awareness</a>
-	
-	<br><hr><a href="../cash-program">CASH Program</a>
-	
-	
-	
-	<br><hr><a href="../fuel-service-form">Fuel Service Form</a>
-	
-	<br><hr><a href="../information-security-and-data-privacy">Information Security and Data Privacy</a>
- 	<br><hr><a href="../heat-illness-prevention-plan">Heat Illness Prevention Plan</a>
+	<br><hr><a href="../spill-response-and-water-protection">Spill Response and Water Protection</a>
+	<br><hr><a href="../technician-environmental-training">Technician Environmental Training</a>
+	<br><hr><a href="../workplace-violence">Workplace Violence</a>
+
 	`
 	
 let stringTitle = ""
